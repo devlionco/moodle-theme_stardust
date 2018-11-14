1e4ae6f68eebbace31b1e000132745b317efd2bb
Nadav Kavalerchik (Davidson/Weizmann) <nadavkav@gmail.com>
2018-11-14 19:31:08 +0200

    layout/incourse.php (patch) add role class

--- layout/incourse.php
+++ layout/incourse.php
@@ -118,6 +118,20 @@ $extraclasses = [];
 if ($navdraweropen) {
     $extraclasses[] = 'drawer-open-left';
 }
+$userrole = ' role-teacher';
+$isstudent = false;
+$userroles = get_user_roles($PAGE->context, $USER->id, true);
+foreach ($userroles as $role) {
+    if ($role->roleid == 5) $isstudent = true;
+}
+if ($isstudent) {
+    $userrole = ' role-student';
+}
+if (has_capability('moodle/site:config', context_system::instance())) {
+    $userrole = ' role-admin';
+}
+$extraclasses[] = $userrole;
+
 $bodyattributes = $OUTPUT->body_attributes($extraclasses);
 $blockshtml = $OUTPUT->blocks('side-pre');
 $hasblocks = strpos($blockshtml, 'data-block=') !== false;
