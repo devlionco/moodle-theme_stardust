63aa7ee9feb3c6c6b0a861304799977f01696630
Alexp <alexp@devlion.co>
2018-11-28 18:23:39 +0200

    T-#326 block menu

--- javascripts/blockslider.js
+++ javascripts/blockslider.js
@@ -1,20 +1,55 @@
-$('#blocksliderbutton').click(function(){
+const windowWidth = $(window).width();
+const blockWidth = $(".card-block").width();
+const curentLang = document
+    .querySelector("html")
+    .dir;
+const margin = (windowWidth - blockWidth) / 2;
 
-    $('.slidingblockpanel').animate({'right':'-1000px'});   
-       $(this).toggleClass('blocksliderbutton_open');
+$(document).ready(function () {
+    if (curentLang == "ltr") {
+    
+        $(".slidingblockpanel").animate({right: "-1000px"});
+    } else {
+        $(".slidingblockpanel").animate({left: "-1000px"});
+  
+    }
+});
 
-   if($('#blocksliderbutton').hasClass('blocksliderbutton_open')) {
- 
+$("#blocksliderbutton").click(function () {
+    $(this).toggleClass("blocksliderbutton_open");
+
+    if ($("#blocksliderbutton").hasClass("blocksliderbutton_open")) {
+        var rightMarg = {};
+
+        
 
         var theOffset = $(this).offset();
-       $('body,html').animate({
-       scrollTop: theOffset.top - 50
-       }, 400); 
-       $('.slidingblockpanel').animate({'right':'0px'}, 800);   
-   } else {
-    $('.slidingblockpanel').animate({'right':'-1000px'});   
-   }
-      
-       $(this).find('i').delay(550).toggleClass('fa fa-bars');
-       $(this).find('i').delay(550).toggleClass('fa fa-times');
-   });
+
+        $("body,html").animate({
+            scrollTop: theOffset.top - 50
+        }, 400);
+
+        if (curentLang == "ltr") {
+            rightMarg.right = -margin;
+            $(".slidingblockpanel").animate(rightMarg, 800);
+        } else {
+            rightMarg.left = -margin;
+            $(".slidingblockpanel").animate(rightMarg, 800);
+        }
+    } else {
+        if (curentLang == "ltr") {
+            $(".slidingblockpanel").animate({right: "-1000px"});
+        } else {
+            $(".slidingblockpanel").animate({left: "-1000px"});
+        }
+    }
+
+    $(this)
+        .find("i")
+        .delay(550)
+        .toggleClass("fa fa-bars");
+    $(this)
+        .find("i")
+        .delay(550)
+        .toggleClass("fa fa-times");
+});
