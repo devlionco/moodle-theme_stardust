5e531b4868ae1f0d1aae2d330f20e86db0ccdc41
Ilya Dymshits <ilya@devlion.co>
2019-07-02 01:59:23 +0300

    version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2019052012;
+$plugin->version   = 2019052014;
 $plugin->requires  = 2017110800;
 $plugin->component = 'theme_stardust';
 $plugin->dependencies = array(
