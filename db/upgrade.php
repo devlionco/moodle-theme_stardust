5ae455460c45f8b895573ed0ed18d4a55e0f918e
Ilya Dymshits <ilya@devlion.co>
2019-07-02 01:25:55 +0300

    update version

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,15 +1,15 @@
 <?php
- 
+
 function xmldb_theme_stardust_upgrade($oldversion) {
     global $CFG, $DB;
 
     $dbman = $DB->get_manager();
- 
+
     if ($oldversion < 2018091901) {
 
         // Define table theme_stardust_messages to be created.
         $table = new xmldb_table('theme_stardust_messages');
-        
+
         // Adding fields to table theme_stardust_messages.
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
@@ -17,15 +17,15 @@ function xmldb_theme_stardust_upgrade($oldversion) {
         $table->add_field('userfrom', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
         $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
         $table->add_field('timevalidbefore', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
-        
+
         // Adding keys to table theme_stardust_messages.
         $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-        
+
         // Conditionally launch create table for theme_stardust_messages.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
-        
+
         // Stardust savepoint reached.
         upgrade_plugin_savepoint(true, 2018091901, 'theme', 'stardust');
     }
@@ -51,33 +51,33 @@ function xmldb_theme_stardust_upgrade($oldversion) {
         // Stardust savepoint reached.
         upgrade_plugin_savepoint(true, 2018092802, 'theme', 'stardust');
     }
-        
-    if ($oldversion < 2019052000) {
+
+    if ($oldversion < 2019052010) {
 
         // Define table theme_stardust_reminders to be created.
         $table = new xmldb_table('theme_stardust_reminders');
-        
+
         // Adding fields to table theme_stardust_reminders.
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('text', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
         $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
         $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
         $table->add_field('timeremind', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
-        
+
         // Adding keys to table theme_stardust_reminders.
         $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-        
+
         // Conditionally launch create table for theme_stardust_reminders.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
-        
+
         // Stardust savepoint reached.
         upgrade_plugin_savepoint(true, 2019052000, 'theme', 'stardust');
     }
-    
+
     $result = TRUE;
- 
+
     return $result;
 }
 ?>
